42b5e609dc997fc9a6465e8c8e25de5fa7c0d4e9
wolfencodierung <pr.ccruz@gmail.com>
2018-04-09 03:55:20 -0400

    New Repo

---
+++ src/date.js
@@ -0,0 +1,7 @@
+$('time').each(function(i, e) {
+    var time = moment($(e).attr('datetime'));
+
+    if(now.diff(time, 'days') <= 1) {
+        $(e).html('<span>' + time.from(now) + '</span>');
+    }
+});
