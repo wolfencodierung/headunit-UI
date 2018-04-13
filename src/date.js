70cba8489dc9409b9818b578c65742b378a83fa6
wolfencodierung <pr.ccruz@gmail.com>
2018-04-12 23:02:54 -0400

    added musicplayer

--- src/date.js
+++ src/date.js
@@ -1,12 +1,16 @@
-var sep = "  "; 
-var ampm;
-var d = new Date();
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-var wkDay =  days[d.getDay()];
-var month = months[d.getMonth()];
-var mDay = d.getDate();
-var year = d.getFullYear();
-
-document.getElementById("date1").innerHTML = wkDay;
-document.getElementById("date2").innerHTML =  month + sep + mDay + "," + sep + year + sep;
+        var d = new Date();
+        var sep = " "
+        var sep2 = ", "
+        
+        /* Date Section */
+        var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        
+        var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        var wkDay = days[d.getDay()];
+        var month = months[d.getMonth()];
+        var mDay = d.getDate();
+        var year = d.getFullYear();
+        
+        document.getElementById("date1").innerHTML = wkDay;
+        document.getElementById("date2").innerHTML = month + sep + mDay + sep2 + year;
+        
