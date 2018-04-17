7efc399eba9a80ca4e8c95df3e2e718490c360d0
wolfencodierung <pr.ccruz@gmail.com>
2018-04-17 09:40:32 -0400

    rework

---
+++ src/aplayer.js
@@ -0,0 +1,4 @@
+function setToActive() {
+    document.getElementById("nav-element-1").classList.add("link-active");
+    document.getElementById("nav-element-1").classList.remove("link-normal");
+    }
